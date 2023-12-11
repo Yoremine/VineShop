f2ead130d6272518db681f0bf43601db460dbcf0
L4pki <79217462+L4pki@users.noreply.github.com>
2023-12-11 17:15:36 +0300

    Valid

--- form.js
+++ form.js
@@ -9,8 +9,24 @@ console.log(`Улица = ${profileStreet.placeholder}`);
 
 let buttonProfile = document.querySelector('.form__button');
 
+var nameErr =  true;
+var mobileErr =  true;
+
+function printError(elemId, hintMsg) {
+  document.getElementById(elemId).innerHTML = hintMsg;
+}
+
 function showClick() {
+  var regexName = /^[А-Яа-яЁё\s]+$/;
+  if(regexName.test(profileName.value) === false) {
+  printError("nameErr", "Пожалуйста, используйте кириллицу ");
+  }
+  var regexPhone = /[+]7\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}/;
+  if(regexPhone.test(profilePhone.value) === false) {
+  printError("mobileErr", "Пожалуйста, введите 10-значный номер мобильного телефона формат  +7 (777) 777-77-77");
+  }
   console.log(`Имя = ${profileName.value}\nТелефон = ${profilePhone.value}\nУлица = ${profileStreet.value}`);
+
 }
 
 buttonProfile.addEventListener('click', showClick);
